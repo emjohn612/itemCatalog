f8c4e7f46767f973f4dc4accdbdaf13b006f3294
Eric M Johnston <emjohn612@gmail.com>
2018-04-11 04:34:56 -0400

    addition of datetime

--- database_setup.py
+++ database_setup.py
@@ -1,17 +1,19 @@
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, backref
 from sqlalchemy import create_engine
 
 Base = declarative_base()
 
+
 class User(Base):
     __tablename__ = 'user'
 
-    name = Column(String(250), nullable = False)
-    id = Column(Integer, primary_key = True)
-    email = Column(String(250), nullable = False)
+    name = Column(String(250), nullable=False)
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
     picture = Column(String(250))
+
 
 class Category(Base):
     __tablename__ = 'category'
@@ -23,31 +25,33 @@ class Category(Base):
 
     @property
     def serialize(self):
-       return {
-           'name': self.name,
-           'id': self.id,
-       }
+        return {
+            'name': self.name,
+            'id': self.id,
+        }
+
 
 class CategoryItem(Base):
     __tablename__ = 'item'
 
-
-    name =Column(String(80), nullable = False)
-    id = Column(Integer, primary_key = True)
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
     description = Column(String(250))
-    category_id = Column(Integer,ForeignKey('category.id'))
-    category = relationship(Category)
+    date = Column(DateTime, nullable=False)
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Category, backref=backref(
+        "children", cascade="all,delete"))
+    #category = relationship(Category)
     user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(User)
 
-
     @property
     def serialize(self):
-       return {
-           'name': self.name,
-           'description': self.description,
-           'id': self.id,
-       }
+        return {
+            'name': self.name,
+            'description': self.description,
+            'id': self.id,
+        }
 
 
 engine = create_engine('sqlite:///itemCatalog.db')
