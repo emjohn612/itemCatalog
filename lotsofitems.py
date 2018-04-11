bc4714e4fe3624d495fa56b263ee69a8e8e0368f
Eric M Johnston <emjohn612@gmail.com>
2018-04-11 04:35:16 -0400

    addition of datetime

--- lotsofitems.py
+++ lotsofitems.py
@@ -1,21 +1,14 @@
 from sqlalchemy import create_engine
+import datetime
 from sqlalchemy.orm import sessionmaker
-
 from database_setup import Category, Base, CategoryItem, User
 
 engine = create_engine('sqlite:///itemCatalog.db')
-# Bind the engine to the metadata of the Base class so that the
-# declaratives can be accessed through a DBSession instance
+
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
-# A DBSession() instance establishes all conversations with the database
-# and represents a "staging zone" for all the objects loaded into the
-# database session object. Any change made against the objects in the
-# session won't be persisted into the database until you call
-# session.commit(). If you're not happy about the changes, you can
-# revert all of them back to the last commit by calling
-# session.rollback()
+
 session = DBSession()
 
 
@@ -32,20 +25,20 @@ session.add(category1)
 session.commit()
 
 categoryItem1 = CategoryItem(user_id=1, name="Goggles", description="Protect your eyes from the sun and snow while riding down the slopes.",
-                     category=category1)
+                             category=category1, date=datetime.datetime.now())
 
 session.add(categoryItem1)
 session.commit()
 
 
 categoryItem2 = CategoryItem(user_id=1, name="Jacket", description="Keep warm with a well insulated coat.",
-                     category=category1)
+                             category=category1, date=datetime.datetime.now())
 
 session.add(categoryItem2)
 session.commit()
 
-categoryItem3 = CategoryItem(user_id=1, name="Snowboard", description="Several types; freestyle, freeride, all terrain, camber, rocker, etc. It is best to find a board that suits your style of riding.",
-                     category=category1)
+categoryItem3 = CategoryItem(user_id=1, name="Snowboard", description="Several types: freestyle, freeride, all terrain, camber, rocker, etc. It is best to find a board that suits your style of riding.",
+                             category=category1, date=datetime.datetime.now())
 
 session.add(categoryItem3)
 session.commit()
@@ -57,25 +50,25 @@ category2 = Category(user_id=1, name="Soccer")
 session.add(category2)
 session.commit()
 
-categoryItem1 = CategoryItem(user_id=1, name="Soccer Ball", description="Different types of soccer balls; turf, trainging ball, and a match ball. ",
-                     category=category2)
+categoryItem1 = CategoryItem(user_id=1, name="Soccer Ball", description="There are different types of soccer balls: turf, training ball, and a match ball. ",
+                             category=category2, date=datetime.datetime.now())
 
 session.add(categoryItem1)
 session.commit()
 
 
 categoryItem2 = CategoryItem(user_id=1, name="Shin Guards", description="Protect your shins from unwanted contact.",
-                     category=category2)
+                             category=category2, date=datetime.date
+                             time.now())
 
 session.add(categoryItem2)
 session.commit()
 
-categoryItem3 = CategoryItem(user_id=1, name="Cleats", description="A well contructed cleat can go a long way. Better ball controll, more explosive power/speed, and flexability.",
-                     category=category2)
+categoryItem3 = CategoryItem(user_id=1, name="Cleats", description="A well contructed cleat can go a long way. Better ball controll, more explosive power/speed, and flexability/comfort.",
+                             category=category2, date=datetime.datetime.now())
 
 session.add(categoryItem3)
 session.commit()
-
 
 
 print "added category items!"
